31b1bf17ae7e5456dcee7327f14ec7658bed7466
Chris Allen <cehallen@gmail.com>
2014-07-22 16:59:15 -0500

    Before sendgrid

---
+++ README.md
@@ -0,0 +1,8 @@
+sendgrid
+u: callen@thisisfusion.com
+p: Fusion0722!
+
+flickr
+u: plawnenforcement@yahoo.com
+p: Password1
+
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page */
 router.get('/', function(req, res) {
   res.render('index', { index: true });
 });
+
+/* GET contact page */
+router.get('/contact', function(req, res) {
+  res.render('contact', { contact: true });
+});
+
+/* GET gallery page */
+router.get('/gallery', function(req, res) {
+  res.render('gallery', { gallery: true });
+});
+
+/* GET services page */
+router.get('/services', function(req, res) {
+  res.render('services', { services: true });
+});
+
+/* GET faq page */
+router.get('/faq', function(req, res) {
+  res.render('faq', { faq: true });
+});
+
+/* POST email sending */
+// router.post('/send-inquiry', function(req, res) {
+//   res.send('')
+// });
 
 module.exports = router;
