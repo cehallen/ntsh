3c3431bcd66fa05dda9fe9087435232dad466c0b
Chris Allen <cehallen@gmail.com>
2014-07-24 15:25:33 -0500

    crap version of message error done

--- routes/index.js
+++ routes/index.js
@@ -30,41 +30,72 @@ router.get('/contact', function(req, res) {
   res.render('contact', { contact: true });
 });
 
+//added
 // console.log(validator.isEmail('kjsdf@sdf'));
 // console.log(validator.isLength('notso loong string', 0, 100));
 function validate(message) {
   errors = [];
-  if (!validator.isLength(message.name, 0, 100)) {
-    errors.push('Name must be 0 to 100 characters');
+  if (!validator.isLength(message.name, 1, 100)) {
+    errors.push(" name must be 1 to 100 characters");
   }
-  if (!validator.isLength(message.subject, 0, 100)) {
-    errors.push('Subject must be 0 to 100 characters');
+  if (!validator.isLength(message.subject, 1, 100)) {
+    errors.push(" subject must be 1 to 100 characters");
   }
-  if (!validator.isLength(message.message, 0, 1000)) {
-    errors.push('Message must be 0 to 1000 characters');
+  if (!validator.isLength(message.message, 1, 1000)) {
+    errors.push(" message must be 1 to 1000 characters");
   }
   if (!validator.isEmail(message.email)) {
-    errors.push('Invalid email');
+    errors.push(" invalid email");
   }
+  return errors;
 }
 
-// ADDED
-  /* POST email sending */
-  router.post('/contact-send', function(req, res) {
-    var message = req.body.message;
+//
+
+
+
+/* POST email sending */
+router.post('/contact', function(req, res) {
+  var message = req.body.message;
+  var errors = validate(message);
+  var locals = {};
+
+  // console.log(errors);
+
+  if (errors.length === 0) {
     var email = new sendgrid.Email({
       to: process.env.EMAIL_RECIPIENT,
       fromname: message.name,
       from: message.email,
       subject: message.subject,
-      text: message.message
+      text: message.message,
     });
     sendgrid.send(email, function(err, json) {
-      if (err) { return console.error(err); }
-      console.log(json);
+      if (err) {
+        // return console.error(err);
+        locals.error = 'Error sending message';
+        // locals.message = message;
+      }
+      locals.notice = 'Your message has been sent';
+      // console.log(json);
     });
-    res.redirect('/');
-  });
+    console.log(locals);
+    console.log("The email was sent");
+    res.render('contact', { contact: true, result: locals, message: message } );
+
+  } else {
+    // console.log(errors);
+    locals.notice = 'Your message has errors:';
+    locals.errors = errors;
+    // locals.message = message;
+    console.log("Your message has errors");
+    console.log(locals);
+    console.log(message);
+    res.render('contact', { contact: true, result: locals, message: message } );
+    // { contact: true, result: locals, message: message }
+  }
+
+});
 
 
 
